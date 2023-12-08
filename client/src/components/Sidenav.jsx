ecd93d84f68caf154c85de38d185377702e2f300
Santos Odigie <santosodigie104@gmail.com>
2023-12-08 12:39:19 +0000

    Finalize contact form and integrate with GetForm.io for submissions

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -22,6 +22,7 @@ const Sidenav = () => {
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -29,6 +30,7 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -36,6 +38,7 @@ const Sidenav = () => {
             <span className="pl-4">Work</span>
           </a>
           <a
+            onClick={handleNav}
             href="#project"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -43,6 +46,7 @@ const Sidenav = () => {
             <span className="pl-4">Projects</span>
           </a>
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -50,6 +54,7 @@ const Sidenav = () => {
             <span className="pl-4">Resume</span>
           </a>
           <a
+            onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
